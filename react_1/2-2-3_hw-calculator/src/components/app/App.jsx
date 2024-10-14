1d6aea350b48253f95bc6bf2fcdb6ff8d9c244d6
maxvanny2010 <mailtime2010@gmail.com>
2024-10-14 16:35:33 +0100

    2-2-3_hw-calculator

--- react_1/2-2-3_hw-calculator/src/components/app/App.jsx
+++ react_1/2-2-3_hw-calculator/src/components/app/App.jsx
@@ -2,21 +2,23 @@ import React from 'react';
 import img from '../../assets/calculator.png';
 import styles from './App.module.css';
 
+const NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const action = ['C', '+', '-', '='];
+
 export const App = () => {
-	const NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-	const action = ['C', '+', '-', '='];
 	const [operand1, setOperand1] = React.useState('');
 	const [operand2, setOperand2] = React.useState('');
 	const [operator, setOperator] = React.useState('');
 	const [result, setResult] = React.useState('');
 	const [isGettingResult, setIsGettingResult] = React.useState(false);
 	const [isOperatorClicked, setIsOperatorClicked] = React.useState(false);
-
+	/* clear the operator and operands*/
 	const clearStateOperands = () => {
 		setOperand1('');
 		setOperand2('');
 		setOperator('');
 	};
+	/* calculate and setResult */
 	const getResult = () => {
 		switch (operator) {
 			case '+':
@@ -30,19 +32,26 @@ export const App = () => {
 		}
 		setIsGettingResult(true);
 	};
-
-	const clearAll = () => {
+	/* clear all for 'C' */
+	const clearAllState = () => {
 		setIsOperatorClicked(false);
 		setIsGettingResult(false);
 		clearStateOperands();
 		setResult('');
 	};
 
+	/* to check if operand start with '0'.
+	  if '0' is first element of operand this action will be rejected
+	*/
 	function getPrevValueWithoutFirstZeroNumber(number, prevOperand) {
 		return (number.toString() === '0' && prevOperand.length === 0) ? prevOperand + '' : prevOperand + number.toString();
 	}
 
-	const getOnClick = (number) => {
+	/* choose operand
+	   if the result is. this onClick will be rejected.
+	   if action is (clicked) choose operand 2 if no choose operand 1
+	 */
+	const setOperand = (number) => {
 		if (!isGettingResult) {
 			isOperatorClicked
 				? setOperand2(prevOperand2 =>
@@ -51,8 +60,12 @@ export const App = () => {
 					getPrevValueWithoutFirstZeroNumber(number, prevOperand1));
 		}
 	};
-
-	const callAction = (actionIndex) => {
+	/*
+	  set the operator
+	  1. if result is.
+	  2. if operand 1 is.
+	 */
+	const setOperators = (actionIndex) => {
 		if (isGettingResult) {
 			setIsGettingResult(false);
 			setIsOperatorClicked(true);
@@ -64,15 +77,16 @@ export const App = () => {
 			setIsOperatorClicked(true);
 		}
 	};
-
+	/*  a template for buttons with number 1 2 3 4 5 6 7 8 9 */
 	const toNumberButtons = (number) => (
 		<td key={number}>
 			<button className={`${styles.btn} ${styles.number}`}
-					onClick={() => getOnClick(number)}>
+					onClick={() => setOperand(number)}>
 				{number}
 			</button>
 		</td>
 	);
+	/*  a template for buttons with action +/-  */
 	const operatorButton = (actionItem, callback) => (
 		<td>
 			<button
@@ -84,6 +98,7 @@ export const App = () => {
 			</button>
 		</td>
 	);
+	/*  a template for buttons with 0 */
 	const zeroButton = (number, callback) => (
 		<td colSpan="3">
 			<button className={`${styles.zero} ${styles.number}`}
@@ -92,6 +107,7 @@ export const App = () => {
 			</button>
 		</td>
 	);
+	/*  a template for buttons with = */
 	const operatorResults = (operator, index) => (
 		<td key={index}>
 			<button id={operator}
@@ -108,7 +124,7 @@ export const App = () => {
 			</button>
 		</td>
 	);
-
+	/*  to display the result in the calculator */
 	let display = <>{
 		isGettingResult
 			? result
@@ -128,20 +144,20 @@ export const App = () => {
 					<tbody>
 					<tr>
 						{NUMS.slice(7, 10).map(toNumberButtons)}
-						{operatorButton(action[0], () => clearAll())}
+						{operatorButton(action[0], () => clearAllState())}
 					</tr>
 
 					<tr>
 						{NUMS.slice(4, 7).map(toNumberButtons)}
-						{operatorButton(action[1], () => callAction(1))}
+						{operatorButton(action[1], () => setOperators(1))}
 					</tr>
 
 					<tr>
 						{NUMS.slice(1, 4).map(toNumberButtons)}
-						{operatorButton(action[2], () => callAction(2))}
+						{operatorButton(action[2], () => setOperators(2))}
 					</tr>
 					<tr>
-						{zeroButton(0, () => getOnClick(0))}
+						{zeroButton(0, () => setOperand(0))}
 						{operatorResults(action[3], 3)}
 					</tr>
 					</tbody>
