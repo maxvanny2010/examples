52e6ca73cd097bec2f64d4283c92ee6b71050774
maxvanny2010 <mailtime2010@gmail.com>
2024-11-01 12:49:35 +0000

    2-2-11 Blog init fonts

---
+++ react_1/2-2-11_blog/README.md
@@ -0,0 +1,8 @@
+npm i -g json-server
+npm i
+styled-components
+prop-types
+react-hook-form yup
+react-router
+redux redux-thunk react-redux
+
---
+++ react_1/2-2-11_blog/src/App.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+export default function App() {
+
+	return (
+		<>
+			<i className="fa fa-camera-retro"></i>
+			<Div>123</Div>
+		</>
+	);
+};
+
+
+
