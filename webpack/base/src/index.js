3e145fd355d395e4f47b884f88e9a022eda89f26
maxvanny2010 <mailtime2010@gmail.com>
2025-01-26 21:06:21 +0000

    webpack devtool source-map

--- webpack/base/src/index.js
+++ webpack/base/src/index.js
@@ -3,6 +3,7 @@ const root = document.querySelector('#app');
 function renderItem(item) {
 	const li = document.createElement('li');
 	li.textContent = item.title;
+	console.log('Working');
 	root.append(li);
 }
 
