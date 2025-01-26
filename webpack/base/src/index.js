b9c76a11dccdd1ef3e0d88b9411b4c3be3d2fae8
maxvanny2010 <mailtime2010@gmail.com>
2025-01-26 23:26:32 +0000

    webpack DevServer serve

--- webpack/base/src/index.js
+++ webpack/base/src/index.js
@@ -1,10 +1,13 @@
-import data from './data'
-import './index.scss'
+import data from './data';
+import './index.scss';
+
 const root = document.querySelector('#app');
-function renderItem(item) {
+
+function renderItem(item, index) {
 	const li = document.createElement('li');
 	li.textContent = item.title;
 	console.log('Working');
+	if (index % 2 === 0) li.style.color = 'red';
 	root.append(li);
 }
 
--- webpack/base/webpack.config.dev.js
+++ webpack/base/webpack.config.dev.js
@@ -1,6 +1,15 @@
 const { merge } = require('webpack-merge');
+const path = require('path');
 const commonConfig = require('./webpack.config.common');
 module.exports = merge(commonConfig, {
 	mode: 'development',
 	devtool: 'inline-source-map',
+	devServer: {
+		port: 3000,
+		hot: true,
+		open: true,
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
+	},
 });
