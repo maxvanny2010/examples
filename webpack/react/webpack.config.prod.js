42d89393f588f122981ae17041745b7a73e10e8a
maxvanny2010 <mailtime2010@gmail.com>
2025-01-27 00:54:54 +0000

    webpack webpack-bundle-analyzer npx webpack --json > stat.json

--- webpack/react/webpack.config.prod.js
+++ webpack/react/webpack.config.prod.js
@@ -1,10 +1,18 @@
 const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.config.common');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const commonConfig = require('./webpack.config.common');
 module.exports = merge(commonConfig, {
 	mode: 'production',
 	devtool: 'source-map',
+	plugins: [
+		new BundleAnalyzerPlugin({
+			analyzerMode: 'static',
+			openAnalyzer: false,
+			reportFilename: 'report.html',
+		}),
+	],
 	optimization: {
 		minimize: true,
 		minimizer: [
