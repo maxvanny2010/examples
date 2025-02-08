b3348818c2eae1173fd89ff5f45ec681813ac663
maxvanny2010 <mailtime2010@gmail.com>
2025-02-08 19:38:16 +0000

    m3.typescript.context

--- react-tsc/vite-ts/src/main.tsx
+++ react-tsc/vite-ts/src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { UserProvider } from './context/user-provider.tsx';
+import { UserProvider } from './context';
 import './index.css';
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<UserProvider>
-			<App />
-		</UserProvider>
-	</StrictMode>,
+	<UserProvider>
+		<App />
+	</UserProvider>,
 );
