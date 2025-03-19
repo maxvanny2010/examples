40a30bd7dc28c49086aea4c31755e4c69f350876
maxvanny2010 <mailtime2010@gmail.com>
2025-03-19 04:26:41 -0500

    m4.react.form.input.

--- react_2/4_3_form-input/src/CustomInputs.jsx
+++ react_2/4_3_form-input/src/CustomInputs.jsx
@@ -3,13 +3,24 @@ import { useState } from 'react';
 
 export function CustomInputs() {
 	const [value, setValue] = useState('');
+	const handleChange = (event) => {
+		console.log('###: change');
+		console.dir('###: event', event.target);
+		console.log('###: event', event.target.value);
+		setValue(event.target.value);
+	};
+	const handleSubmit = () => {
+		alert(value);
+	};
 	return (
 		<div className="App">
 			<header className="App-header">
 				<h6>useArray</h6>
 				<div className="button-container">
-					<input type="text" />
-					<button>Submit</button>
+					<input type="text"
+						   onChange={handleChange} />
+					<button onClick={handleSubmit}>Submit</button>
+					<p>{value}</p>
 				</div>
 			</header>
 		</div>
