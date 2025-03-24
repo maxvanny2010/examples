1cfed9b3f14f91041cf1168a6f3c8d53a12ff3a8
maxvanny2010 <mailtime2010@gmail.com>
2025-03-24 16:12:01 -0500

    m4.react.route inner route

--- react_2/4_4_routes/src/App.jsx
+++ react_2/4_4_routes/src/App.jsx
@@ -1,12 +1,12 @@
 import { Link, Route, Routes } from 'react-router-dom';
+import { Book } from './pages/Book.jsx';
 import { Home } from './pages/Home.jsx';
 import { About } from './pages/About.jsx';
-import { BookList } from './pages/BookList.jsx';
 import { Contact } from './pages/Contact.jsx';
-import { Book } from './pages/Book.jsx';
-import './App.css';
 import { NewBook } from './pages/NewBook.jsx';
 import { NotFound } from './pages/NotFound.jsx';
+import { BookList } from './pages/BookList.jsx';
+import './App.css';
 
 export const App = () => {
 	return (<>
@@ -22,16 +22,19 @@ export const App = () => {
 			</div>
 		</div>
 		<Routes>
+			<!-- Wrong Routes don't support -> <h1></h1>-->
 			<Route path="/"
 				   element={<Home />}></Route>
 			<Route path="/about"
 				   element={<About />}></Route>
-			<Route path="/books"
-				   element={<BookList />}></Route>
-			<Route path="/books/:id/:img?"
-				   element={<Book />}></Route>
-			<Route path="/books/new"
-				   element={<NewBook />}></Route>
+			<Route path="/books">
+				<Route index
+					   element={<BookList />}></Route>
+				<Route path=":id"
+					   element={<Book />}></Route>
+				<Route path="new"
+					   element={<NewBook />}></Route>
+			</Route>
 			<Route path="/contact"
 				   element={<Contact />}></Route>
 			<Route path="*"
