ff5eef9dde90adb652db7e8e504c2e629871c5ba
maxvanny2010 <mailtime2010@gmail.com>
2025-03-26 15:39:54 -0500

    m4.react.route Navigate useNavigate

--- react_2/4_4_routes/src/pages/NotFound.jsx
+++ react_2/4_4_routes/src/pages/NotFound.jsx
@@ -1,3 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 export function NotFound() {
+	const navigate = useNavigate();
+	// const isUser = false;
+	useEffect(() => {
+		setTimeout(() => {
+			navigate('/');
+			//navigate(-2);// page a 2 steps before
+		}, 1000);
+	}, [navigate]);
+	// if (!isUser) return <Navigate to="/" />;
 	return (<h1>NotFound</h1>);
+	// return (<Navigate to="/" />);
 }
