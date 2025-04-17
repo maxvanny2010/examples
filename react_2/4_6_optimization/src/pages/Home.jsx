895a63c07a8d7b84ff6f6040b1f98b8277fc07a3
maxvanny2010 <mailtime2010@gmail.com>
2025-04-17 06:33:30 -0500

    m4.react.optimisation dynamic import function

--- react_2/4_6_optimization/src/pages/Home.jsx
+++ react_2/4_6_optimization/src/pages/Home.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
-import { sum } from '../util';
 import { Admin } from '../component';
 
 export function Home() {
 	const [admin, setAdmin] = useState(false);
+	const handleClick = async () => {
+		const { sum } = await import(/*@vite-ignore*/'../lazy/sum.jsx');
+		alert(sum(2, 2));
+	};
 	return (
 		<>
 			<h1>Home</h1>
-			<button onClick={() => alert(sum(2, 2))}>Plus 2+2</button>
+			<button onClick={handleClick}>Plus 2+2
+			</button>
 			<button onClick={() => setAdmin(s => !s)}>Toggle Admin</button>
 			{admin ? <Admin /> : <h2>Not Admin</h2>}
 		</>
--- react_2/4_6_optimization/src/util/index.jsx
+++ react_2/4_6_optimization/src/util/index.jsx
@@ -1,4 +1,2 @@
 // eslint-disable-next-line react-refresh/only-export-components
 export * from './internalPaths.jsx';
-// eslint-disable-next-line react-refresh/only-export-components
-export * from './sum.jsx';
