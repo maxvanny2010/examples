119a1aa43f375c33b30162f118004d1b8ede945d
maxvanny2010 <mailtime2010@gmail.com>
2025-04-18 07:56:09 -0500

    m4.react.optimisation startTransition

--- react_2/4_6_optimization/src/pages/Home.jsx
+++ react_2/4_6_optimization/src/pages/Home.jsx
@@ -1,9 +1,10 @@
-import { lazy, Suspense, useState } from 'react';
+import { lazy, Suspense, useState, useTransition } from 'react';
 
 const Admin = lazy(() => import('../component/Admin.jsx'));
 
 export function Home() {
 	const [admin, setAdmin] = useState(false);
+	const [isPending, startTransition] = useTransition();
 
 	const handleClick = async () => {
 		const { sum } = await import(/*@vite-ignore*/'../lazy/sum.jsx');
@@ -14,8 +15,14 @@ export function Home() {
 			<h1>Home</h1>
 			<button onClick={handleClick}>Plus 2+2
 			</button>
-			<button onClick={() => setAdmin(s => !s)}>Toggle Admin</button>
-			<Suspense fallback="Loading...">
+			<button disabled={isPending}
+					onClick={() =>
+						startTransition(() => setAdmin(s => !s))}
+			>
+				Toggle Admin
+			</button>
+			{isPending && <p>isLoading...</p>}
+			<Suspense>
 				{admin ? <Admin /> : <h2>Not Admin</h2>}
 			</Suspense>
 		</>
