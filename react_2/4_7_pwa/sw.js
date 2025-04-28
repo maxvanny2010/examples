f80c0d18dd5b49575db78b1f303772461ce12ebe
maxvanny2010 <mailtime2010@gmail.com>
2025-04-28 16:16:13 -0500

    m4.pwa Service Worker activate

--- react_2/4_7_pwa/sw.js
+++ react_2/4_7_pwa/sw.js
@@ -1,3 +1,6 @@
 self.addEventListener('install', (event) => {
 	console.log('SW has been installed');
 });
+self.addEventListener('activate', (event) => {
+	console.log('SW has been activate');
+});
