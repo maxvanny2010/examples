0255430ff0764b3c4ac77a34bfec7fbe1f2022d6
maxvanny2010 <mailtime2010@gmail.com>
2025-04-28 18:36:26 -0500

    m4.pwa handler install activate fetch remove keys

--- react_2/4_7_pwa/sw.js
+++ react_2/4_7_pwa/sw.js
@@ -1,6 +1,58 @@
+const staticCacheName = 'static-site-v1';
+const ASSETS = [
+	'/',
+	'/index.html',
+	'/css/styles.css',
+	'/css/materialize.min.css',
+	'/js/app.js',
+	'/js/ui.js',
+	'/js/materialize.min.js',
+	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'https://fonts.gstatic.com/s/materialicons/v143/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
+];
 self.addEventListener('install', (event) => {
-	console.log('SW has been installed');
+	event.waitUntil(
+		(async () => {
+			const cache = await caches.open(staticCacheName);
+			for (const asset of ASSETS) {
+				try {
+					const response = await fetch(asset);
+					if (!response.ok) {
+						throw new Error(`HTTP error! status: ${response.status}`);
+					}
+					await cache.put(asset, response);
+					console.log(`Success cache: ${asset}`);
+				} catch (error) {
+					console.error(`Fall cache: ${asset}`, error);
+				}
+			}
+		})(),
+	);
 });
-self.addEventListener('activate', (event) => {
-	console.log('SW has been activate');
+
+
+self.addEventListener('activate', async (event) => {
+	const cachesKeyArr = await caches.keys();
+	console.log('Caches keys:', cachesKeyArr);
+	await Promise.all(
+		cachesKeyArr.filter(key => key !== staticCacheName)
+			.map(key => caches.delete(key)));
+
+});
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then(cacheRes => {
+			return cacheRes || fetch(event.request).then(fetchRes => {
+				return caches.open(staticCacheName).then(cache => {
+					// Cache to only owner site
+					if (event.request.method === 'GET' && event.request.url.startsWith(self.location.origin)) {
+						cache.put(event.request, fetchRes.clone()).then(r => r);
+					}
+					return fetchRes;
+				});
+			});
+		}).catch(() => {
+			return caches.match('/offline.html');
+		}),
+	);
 });
