f1afbef4ed4bc7acf7a953b0649f4695d3ba0500
maxvanny2010 <mailtime2010@gmail.com>
2025-04-28 18:35:22 -0500

    m4.pwa handler

--- react_2/4_7_pwa/js/app.js
+++ react_2/4_7_pwa/js/app.js
@@ -1,5 +1,13 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/sw.js')
-		.then(reg => console.log('SW register', reg))
-		.catch(err => console.log('SW error', err));
+		.then(reg => console.log('SW registered', reg))
+		.catch(err => console.log('SW registration error', err));
+
+	if ('registerProtocolHandler' in navigator) {
+		navigator.registerProtocolHandler(
+			'web+posts',
+			`${window.location.origin}/view-post?postUrl=%s`,
+			'Posts Ninja',
+		);
+	}
 }
