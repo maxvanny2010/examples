e837063c2e5010d3ead2d78b37403dc58e3d67a7
maxvanny2010 <mailtime2010@gmail.com>
2025-05-24 16:14:44 -0500

    m4.state react redux ts redux-thunk

---
+++ react_2/4_9_state_react_redux-thunk/src/App.tsx
@@ -0,0 +1,22 @@
+import { Total } from './Total/Total';
+import { Header } from './Header/Header';
+import { Cart } from './Cart/Cart';
+import './App.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { OrderModal } from './OrderModal/OrderModal';
+
+function App() {
+	return (
+		<Provider store={store}>
+			<div className="App">
+				<Header />
+				<Cart />
+				<Total />
+				<OrderModal />
+			</div>
+		</Provider>
+	);
+}
+
+export default App;
---
+++ react_2/4_9_state_react_redux-thunk/src/redux/store.ts
@@ -0,0 +1,17 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { logActionMiddleware } from './logActionMiddleware';
+import { orderReducer } from './orderReducer';
+import { productsReducer } from './productsReducer';
+import thunkMiddleware from 'redux-thunk';
+
+const rootReducer = combineReducers({
+	products: productsReducer,
+	order: orderReducer,
+});
+
+export const store = createStore(
+	rootReducer,
+	applyMiddleware(thunkMiddleware, logActionMiddleware),
+);
+
+export type RootState = ReturnType<typeof rootReducer>;
