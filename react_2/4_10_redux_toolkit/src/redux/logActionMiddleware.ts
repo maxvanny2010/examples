b3f3278700f24039f6637a37f31f3cc350252cea
maxvanny2010 <mailtime2010@gmail.com>
2025-05-30 06:25:00 -0500

    m4.state.toolkit AnyAction deprecated move to UnknownAction

--- react_2/4_10_redux_toolkit/src/redux/logActionMiddleware.ts
+++ react_2/4_10_redux_toolkit/src/redux/logActionMiddleware.ts
@@ -3,7 +3,7 @@ import { logAction } from '../metrics/logAction';
 import { RootState } from './store';
 
 export const logActionMiddleware: Middleware<{}, RootState> =
-	() => next => (action: unknown) => {
+	() => next => action => {
 		logAction(action as UnknownAction);
 		return next(action);
 	};
