5ddbf818d95e592ac1f1c043e7fbad4f0bea6994
maxvanny2010 <mailtime2010@gmail.com>
2025-06-13 06:20:10 -0500

    m4.state.toolkit.hw fix header mb

--- react_2/4_16_redux_toolkit_hw/src/components/MainMenu.tsx
+++ react_2/4_16_redux_toolkit_hw/src/components/MainMenu.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
 export const MainMenu = () => {
 	return (
 		<Navbar bg="light"
-				expand="lg">
+				expand="lg"
+				className="mb-2 shadow-sm rounded text-shadow"
+		>
 			<Container>
-				<Navbar.Brand href="/"><h1>Book Contacts</h1></Navbar.Brand>
+				<Navbar.Brand as={Link} to="/"><h1>Book Contacts</h1></Navbar.Brand>
 				<Nav className="me-auto">
 					<Nav.Link as={NavLink} to="/groups">Groups</Nav.Link>
 					<Nav.Link as={NavLink} to="/favorit">Favorite</Nav.Link>
