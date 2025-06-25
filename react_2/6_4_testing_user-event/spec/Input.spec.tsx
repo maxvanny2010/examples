ce06433924bc40494b3e18daf0c0ae2353c698ba
maxvanny2010 <mailtime2010@gmail.com>
2025-06-25 07:11:34 -0500

    m6.testing user-event

---
+++ react_2/6_4_testing_user-event/spec/Input.spec.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import { Input } from '../src/components/Input';
+
+import uE from '@testing-library/user-event';
+
+describe('Поле ввода', () => {
+	const userEvent = uE.setup({ delay: 100 });
+
+	it.todo('Ограничение на ввод более 32 символов');
+
+	it('Поле доступно для ввода', async () => {
+		const fn = jest.fn();
+
+		render(<Input defaultValue=""
+					  onChange={fn} />);
+
+		const input = screen.getByRole('textbox');
+
+		await userEvent.click(input);
+		await userEvent.keyboard('Hello!');
+
+		expect(fn).toBeCalledWith('Hello!');
+
+		await userEvent.type(input, 'Hello!');
+		await userEvent.tripleClick(input);
+		await userEvent.copy();
+		await userEvent.click(input);
+		await userEvent.paste();
+		screen.debug();
+		expect(screen).getByDisplayValue(/'Hello!Hello!'/).toBeInTheDocument();
+	});
+
+});
---
+++ react_2/6_4_testing_user-event/src/components/Input.tsx
@@ -0,0 +1,47 @@
+import { ChangeEvent, useState } from 'react';
+
+import './styles.css';
+
+type Props = {
+	defaultValue: string,
+	onChange: (value: string) => void;
+}
+
+export const Input = ({ defaultValue, onChange }: Props) => {
+	const [labelValue, setlabelValue] = useState('');
+	const [inputValue, setinputValue] = useState(defaultValue);
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const val = event.target.value;
+
+		if (val.length > 32) {
+			setlabelValue(`Превышена максимальная длина заголовка ${val.length}`);
+		} else {
+			setlabelValue('');
+			setinputValue(val);
+			onChange(val);
+		}
+	};
+
+	return (
+		<div className="input-field">
+			<label
+				htmlFor="input-header-field-id"
+				data-testid="input-label"
+			>
+				Заголовок задачи
+			</label>
+			<input
+				id="input-header-field-id"
+				type="text"
+				value={inputValue}
+				onChange={handleChange}
+				placeholder={`например, введите "купить молоко"`}
+				alt="поле для ввода заголовка задачи"
+				title="поле для заголовка"
+				data-testid="input-field"
+			/>
+			<span data-testid="input-hint-text">{labelValue}</span>
+		</div>
+	);
+};
