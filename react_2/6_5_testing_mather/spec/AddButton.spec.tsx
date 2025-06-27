4a398c510241cfa14bc18690725c0d0a5953dc2f
maxvanny2010 <mailtime2010@gmail.com>
2025-06-27 06:47:35 -0500

    m6.testing matcher

---
+++ react_2/6_5_testing_mather/spec/AddButton.spec.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { AddButton } from '../src/components/AddButton';
+import { userEvent } from '@testing-library/user-event/setup/index';
+
+describe('Кнопка "Добавить"', () => {
+
+	it.todo('Блокировка для строки больше 32 символов');
+	it.todo('Блокировка для строки меньше 1 символа');
+
+	it('Проверка кликов по кнопке', async () => {
+		const fn = jest.fn();
+		render(<AddButton onClick={fn} />);
+		const button = screen.getByText(/добавить/i);
+		userEvent.click(button);
+		expect(fn).toBeCalledTimes(1);
+	});
+});
---
+++ react_2/6_5_testing_mather/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { Input } from './components/Input';
+import { AddButton } from './components/AddButton';
+import './styles.css';
+
+export const App = () => {
+	const [inputValue, setinputValue] = useState('');
+
+	const handleAddButtonClick = () => {
+		console.log('Button was clicked!');
+	};
+
+	return (
+		<div className="root-container">
+			<Input
+				defaultValue={inputValue}
+				onChange={(val) => setinputValue(val)}
+			/>
+			<AddButton onClick={handleAddButtonClick} />
+		</div>
+	);
+};
