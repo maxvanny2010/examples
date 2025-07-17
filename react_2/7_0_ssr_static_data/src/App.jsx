b2f9b621c7929e6c21c75d7c81deb7584170350e
maxvanny2010 <mailtime2010@gmail.com>
2025-07-17 04:30:01 -0500

    m7.ssr.static.render.data.props.wrong.way.evidence

--- react_2/7_0_ssr_static_data/src/App.jsx
+++ react_2/7_0_ssr_static_data/src/App.jsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
+
 export const App = ({ data = [] }) => {
+	const [count, setCount] = useState(0);
 	return (
-		<ul>
-			{
-				data.map(({ name, mark }) => (
-					<li key={name}>{name}-{mark}</li>
-				))
-			}
-		</ul>
+		<div>Count - {count}
+			<button onClick={() => setCount(prev => prev + 1)}>Click</button>
+			<ul>
+				{
+					data.map(({ name, mark }) => (
+						<li key={name}>{name}-{mark}</li>
+					))
+				}
+			</ul>
+		</div>
 	);
 };
 
@@ -17,6 +23,6 @@ const data = [
 ];
 
 App.getServerSideProps = async () => {
-	await new Promise((r) => setTimeout(r, 1000));
+	await new Promise((r) => setTimeout(r, 2000));
 	return data;
 };
