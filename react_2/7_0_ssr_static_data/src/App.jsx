b4d1a56422591eb73ccc62f99bab8e2ae03797d2
maxvanny2010 <mailtime2010@gmail.com>
2025-07-17 04:52:54 -0500

    m7.ssr.static.render.data.props.wrong.way.getTime()

--- react_2/7_0_ssr_static_data/src/App.jsx
+++ react_2/7_0_ssr_static_data/src/App.jsx
@@ -17,7 +17,7 @@ export const App = ({ data = [] }) => {
 };
 
 const data = [
-	{ name: 'TypeScript', mark: '4.9' },
+	{ name: 'TypeScript', mark: new Date().getSeconds() },
 	{ name: 'JavaScript', mark: '4.8' },
 	{ name: 'Java', mark: '5.0' },
 ];
