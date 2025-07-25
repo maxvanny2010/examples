210060635627970ce7e114bb05e1d69f267d86d1
maxvanny2010 <mailtime2010@gmail.com>
2025-07-25 06:33:38 -0500

    m7.ssr.next.app.component.layout

--- react_2/8_0_ssr_next/pages/_app.tsx
+++ react_2/8_0_ssr_next/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ReactElement, ReactNode } from 'react';
+import { NextPage } from 'next';
 
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} user="ADMIN"/>;
+type NextPageWithLayout = NextPage & {
+	getLayout?: (page: ReactElement) => ReactNode;
+}
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout;
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+	const getLayout = Component.getLayout ?? ((page) => page);
+	return (
+		<>
+			START
+			{getLayout(<Component {...pageProps} user="ADMIN" />)}
+			END
+		</>
+	);
 }
