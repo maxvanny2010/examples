eada2e0f5ad9be31e50390481bb992aed185a06a
maxvanny2010 <mailtime2010@gmail.com>
2025-07-28 06:35:17 -0500

    m7.ssr.next.app.data.getServerSideProps.root

--- react_2/8_3_next_app_data/src/pages/_app.tsx
+++ react_2/8_3_next_app_data/src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import { Header } from '@/components/Header';
+import { TopLoadingBar } from '@/components/TopLoadingBar';
+import { AppProps } from 'next/app';
 import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<div className="min-h-screen bg-gray-100 px-4 sm:px-6">
+			<Header />
+			<TopLoadingBar />
+			<main className="max-w-[1440px] mx-auto">
+				<Component {...pageProps} />
+			</main>
+		</div>
+	);
 }
