678b3e459a73d532e316b0d081fe4ee5fd9112b4
maxvanny2010 <mailtime2010@gmail.com>
2025-08-02 13:59:53 -0500

    m7.ssr.next.app.server-component.api.fetch

--- react_2/8_4_next_app_server_component/src/lib/api.ts
+++ react_2/8_4_next_app_server_component/src/lib/api.ts
@@ -1,17 +1,19 @@
-import { Film, filmSchema } from '@/shema/filmPropertiesSchema';
+import { filmSchema } from '@/shema/filmPropertiesSchema';
 
-export async function getFilmById(id: string): Promise<Film> {
-	console.log('⚠️ FETCH from server', id);
-	const res = await fetch(`https://www.swapi.tech/api/films/${id}`);
+export async function getFilmById(id: string) {
+	const res = await fetch(`https://www.swapi.tech/api/films/${id}`, {
+		cache: 'force-cache',
+		next: { revalidate: 60 },
+	});
+
 	if (!res.ok) throw new Error('Failed to fetch film');
 
 	const json = await res.json();
-	const parseResult = filmSchema.safeParse(json.result);
+	const parsed = filmSchema.safeParse(json.result);
 
-	if (!parseResult.success) {
-		console.error(parseResult.error);
+	if (!parsed.success) {
 		throw new Error('Invalid film data format');
 	}
 
-	return parseResult.data;
+	return parsed.data;
 }
