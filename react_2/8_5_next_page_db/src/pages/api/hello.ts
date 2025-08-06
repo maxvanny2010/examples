1e786a8f76d1f2420b9bfad2b9a75a0ea517f67c
maxvanny2010 <mailtime2010@gmail.com>
2025-08-06 04:37:06 -0500

    m7.ssr.next.page.db.prisma-client.getServerSideProps.use-client

--- react_2/8_5_next_page_db/src/pages/api/hello.ts
+++ react_2/8_5_next_page_db/src/pages/api/hello.ts
@@ -1,19 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import prisma, { User } from '@/server/db';
 
-export type SafeUser = Pick<User, 'id' | 'name' | 'email'>;
 
 export default async function handler(
 	req: NextApiRequest,
-	res: NextApiResponse<SafeUser[]>,
+	res: NextApiResponse<any>,
 ) {
-	const users = await prisma.user.findMany({
-		select: {
-			id: true,
-			name: true,
-			email: true,
-		},
-	});
 
-	res.status(200).json(users);
+	res.status(200);
 }
--- react_2/8_5_next_page_db/src/pages/index.tsx
+++ react_2/8_5_next_page_db/src/pages/index.tsx
@@ -1,17 +1,28 @@
 import { GetServerSideProps } from 'next';
-import { SafeUser } from '@/pages/api/hello';
+import { SafeUser } from '@/types/SafeUser';
+import prisma from '@/server/db';
 
-export default function Home(data: SafeUser[]) {
+
+export default function Home(users: SafeUser[]) {
 	return (
-		<pre>{JSON.stringify(data, null, 2)}</pre>
+		<pre>{JSON.stringify(users, null, 2)}</pre>
 	);
 }
 export const getServerSideProps: GetServerSideProps = async () => {
-	const response = await fetch('http://localhost:3000/api/hello');
-	const data = await response.json();
+	const users = await prisma.user.findMany({
+		select: {
+			id: true,
+			name: true,
+			email: true,
+		},
+	});
 	return {
 		props: {
-			...data,
+			...users,
 		},
 	};
+	//bad way. to use without the request to host
+	/*const response = await fetch('http://localhost:3000/api/hello');
+	const data = await response.json();
+	*/
 };
