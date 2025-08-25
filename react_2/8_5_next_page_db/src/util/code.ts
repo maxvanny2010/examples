b2abf2ebbae2ff4e53b171bf78968c39713bbe1a
maxvanny2010 <mailtime2010@gmail.com>
2025-08-25 07:14:09 -0500

    m7.ssr.next.page.db.code-bad-request

--- react_2/8_5_next_page_db/src/util/code.ts
+++ react_2/8_5_next_page_db/src/util/code.ts
@@ -2,6 +2,7 @@ export const CODE = {
 	UNAUTHORIZED: 'UNAUTHORIZED',
 	FORBIDDEN: 'FORBIDDEN',
 	NOT_FOUND: 'NOT_FOUND',
+	BAD_REQUEST: 'BAD_REQUEST',
 } as const;
 
 export type TRPCErrorCode = typeof CODE[keyof typeof CODE];
