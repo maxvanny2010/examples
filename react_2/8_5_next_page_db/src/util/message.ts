12b5f387b0cadcd7121117e07db82bbd4234576b
maxvanny2010 <mailtime2010@gmail.com>
2025-08-25 07:14:21 -0500

    m7.ssr.next.page.db.messages

--- react_2/8_5_next_page_db/src/util/message.ts
+++ react_2/8_5_next_page_db/src/util/message.ts
@@ -1,10 +1,12 @@
 export const MESSAGES = {
 	USER_NOT_FOUND: 'Пользователь не найден или удалён',
 	EVENT_NOT_FOUND: 'Событие не найдено',
+	EVENT_NO_ID: 'ID события не передан',
 	ADMIN_EMAIL: 'admin@gmail.com',
 	ADMIN_NAME: 'admin',
 	AMIN_EXIST: 'Admin already exists',
 	ADMIN_CREATED: 'Admin user created: admin@gmail.com / admin',
 	ADMIN_FAILED: 'Failed to ensure admin exists:',
+	ADMIN_ONLY: 'Admin only',
 } as const;
 export type Messages = typeof MESSAGES[keyof typeof MESSAGES];
