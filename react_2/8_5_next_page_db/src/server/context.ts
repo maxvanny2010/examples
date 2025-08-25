e5f6051157f50db06e4690ccaed642dae54c25ac
maxvanny2010 <mailtime2010@gmail.com>
2025-08-25 08:19:49 -0500

    m7.ssr.next.page.db.context.types

--- react_2/8_5_next_page_db/src/server/context.ts
+++ react_2/8_5_next_page_db/src/server/context.ts
@@ -1,36 +1,32 @@
-// server/context.ts
 import type { CreateNextContextOptions } from '@trpc/server/adapters/next';
 import type { Session } from 'next-auth';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import prisma from '@/server/db';
 
-// --- Тип пользователя из базы данных (DBUser) ---
+// Тип пользователя из базы данных
 export type DBUser = Session['user'] & {
 	name: string;
 	email: string;
 	deleted: boolean;
 };
 
-// --- Контекст для tRPC ---
-export const createContext = async ({ req, res }: CreateNextContextOptions) => {
+// Контекст для tRPC
+export type Context = {
+	user?: Session['user'];
+	dbUser?: DBUser;
+};
+
+export const createContext = async ({ req, res }: CreateNextContextOptions): Promise<Context> => {
 	const session = await getServerSession(req, res, authOptions);
 
-	if (!session?.user) return { user: undefined };
+	if (!session?.user) return {};
+
 	const dbUser = await prisma.user.findUnique({
 		where: { id: session.user.id },
 	});
-	if (!dbUser || dbUser.deleted) {
-		return { user: undefined };
-	}
 
-	return {
-		user: session.user, // Session['user']
-		dbUser, // DBUser
-	};
+	if (!dbUser || dbUser.deleted) return { user: session.user };
+
+	return { user: session.user, dbUser };
 };
-
-export type Context = Awaited<ReturnType<typeof createContext>>;
-
-// Контекст с dbUser
-export type ContextWithDBUser = Context & { dbUser: DBUser };
