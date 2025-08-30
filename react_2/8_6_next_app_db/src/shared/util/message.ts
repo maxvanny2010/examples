f17568ee1392763c3c410f1eed7e19be873a2184
maxvanny2010 <mailtime2010@gmail.com>
2025-08-30 16:34:11 -0500

    m8.ssr.next.app.message.user-password-not-found

--- react_2/8_6_next_app_db/src/shared/util/message.ts
+++ react_2/8_6_next_app_db/src/shared/util/message.ts
@@ -1,9 +1,8 @@
 export const MESSAGES = {
 	USER_NOT_FOUND: 'User not found or deleted',
+	USER_PASSWORD_NOT_FOUND: 'Invalid email or password. Please try again',
 	EVENT_NOT_FOUND: 'Event not found',
 	EVENT_NO_ID: 'Event ID not provided',
-	ADMIN_EMAIL: 'admin@gmail.com',
-	ADMIN_NAME: 'admin',
 	AMIN_EXIST: 'Admin already exists',
 	ADMIN_CREATED: 'Admin user created: admin@gmail.com / admin',
 	ADMIN_FAILED: 'Failed to ensure admin exists:',
