35ed8a966d69a3a54f698c4270ecba29fc87f070
maxvanny2010 <mailtime2010@gmail.com>
2025-09-02 16:06:13 -0500

    m8.ssr.next.app.state-error

--- react_2/8_6_next_app_db/src/entities/event/state/StateError.tsx
+++ react_2/8_6_next_app_db/src/entities/event/state/StateError.tsx
@@ -2,12 +2,17 @@
 
 import { FaFrown } from 'react-icons/fa';
 
-export const StateError = () => {
+interface ErrorStateProps {
+	message?: string;
+}
+
+export const StateError = ({ message }: ErrorStateProps) => {
 	return (
 		<div className="sm:col-span-2 lg:col-span-4 text-center py-10">
 			<p className="text-lg text-red-500 flex items-center justify-center gap-2">
 				<FaFrown /> An error occurred while loading events.
 			</p>
+			<p>{message}</p>
 			<p className="text-slate-500 mt-2">Please try refreshing the page.</p>
 		</div>
 	);
