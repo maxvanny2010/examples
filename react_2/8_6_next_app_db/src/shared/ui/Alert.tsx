9d776e6b5403717231e1c341400f73fbc04962e7
maxvanny2010 <mailtime2010@gmail.com>
2025-09-02 16:12:19 -0500

    m8.ssr.next.app.components.Alert

--- react_2/8_6_next_app_db/src/shared/ui/Alert.tsx
+++ react_2/8_6_next_app_db/src/shared/ui/Alert.tsx
@@ -12,7 +12,7 @@ export const Alert = ({ children, onClose }: AlertProps) => {
 			className="p-4 bg-red-100 border border-red-400 text-red-700 rounded-md relative"
 			role="alert"
 		>
-			<strong className="font-bold">Ошибка! </strong>
+			<strong className="font-bold">Mistake! </strong>
 			<span className="block sm:inline">{children}</span>
 			<button onClick={onClose}
 					className="absolute top-0 bottom-0 right-0 px-4 py-3"
