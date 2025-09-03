a6a7ba5de3a4bf19c8a69787927170e1e9bbad60
maxvanny2010 <mailtime2010@gmail.com>
2025-09-03 08:32:29 -0500

    m7.ssr.next.page.db.admin.trpc

--- react_2/8_5_next_page_db/src/pages/api/trpc/[trpc].ts
+++ react_2/8_5_next_page_db/src/pages/api/trpc/[trpc].ts
@@ -1,8 +1,8 @@
 import * as trpcNext from '@trpc/server/adapters/next';
-import { appRouter } from '@/server/routes';
-import { createContext } from '@/server/context';
+import {appRouter} from '@/server/routes';
+import {createContext} from '@/server/core/context';
 
 export default trpcNext.createNextApiHandler({
-	router: appRouter,
-	createContext,
+    router: appRouter,
+    createContext,
 });
