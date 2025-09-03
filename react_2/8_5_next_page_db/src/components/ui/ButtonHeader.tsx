1d80ce080453df3f691c85a21d0cdb3586f2f227
maxvanny2010 <mailtime2010@gmail.com>
2025-09-03 08:30:15 -0500

    m7.ssr.next.page.db.components.Alert-ButtonHeader

--- react_2/8_5_next_page_db/src/components/ui/ButtonHeader.tsx
+++ react_2/8_5_next_page_db/src/components/ui/ButtonHeader.tsx
@@ -1,120 +1,114 @@
 import Link from 'next/link';
-import { signOut } from 'next-auth/react';
-import { useRouter } from 'next/router';
-import { PATH } from '@/shared/path';
-import { BUTTON_EVENT_TYPE, ROLES } from '@/shared/types';
-import { hasRole } from '@/server/roles';
+import {signOut} from 'next-auth/react';
+import {useRouter} from 'next/router';
+import {PATH} from '@/shared/path';
+import {BUTTON_EVENT_TYPE, ROLES} from '@/shared/types';
+import {hasRole} from '@/server/core/roles';
 import ButtonEventAction from '@/components/ui/ButtonAction';
-import {
-	AiOutlineDashboard,
-	AiOutlineEdit,
-	AiOutlineHome,
-	AiOutlineLogout,
-	AiOutlinePlusCircle,
-} from 'react-icons/ai';
+import {AiOutlineDashboard, AiOutlineEdit, AiOutlineHome, AiOutlineLogout, AiOutlinePlusCircle,} from 'react-icons/ai';
 
 interface ButtonHeaderProps {
-	user: {
-		role: typeof ROLES[keyof typeof ROLES];
-		id?: number;
-		name?: string;
-		email?: string;
-	};
-	eventAuthorId?: number;
+    user: {
+        role: typeof ROLES[keyof typeof ROLES];
+        id?: number;
+        name?: string;
+        email?: string;
+    };
+    eventAuthorId?: number;
 }
 
-export default function ButtonHeader({ user, eventAuthorId }: ButtonHeaderProps) {
-	const router = useRouter();
-	const eventId = Array.isArray(router.query.id) ? router.query.id[0] : router.query.id;
-	console.log(eventAuthorId);
-	const isDetailPage = Boolean(eventId) && router.pathname === PATH.EVENTS.ID('[id]');
-	const isHome = router.pathname === PATH.HOME.ROOT;
-	const isDashboard = router.pathname === PATH.ADMIN.DASHBOARD;
-	const isSignIn = router.pathname === PATH.AUTH.SIGNIN;
-	const isRegister = router.pathname === PATH.AUTH.REGISTER;
+export default function ButtonHeader({user, eventAuthorId}: ButtonHeaderProps) {
+    const router = useRouter();
+    const eventId = Array.isArray(router.query.id) ? router.query.id[0] : router.query.id;
+    console.log(eventAuthorId);
+    const isDetailPage = Boolean(eventId) && router.pathname === PATH.EVENTS.ID('[id]');
+    const isHome = router.pathname === PATH.HOME.ROOT;
+    const isDashboard = router.pathname === PATH.ADMIN.DASHBOARD;
+    const isSignIn = router.pathname === PATH.AUTH.SIGNIN;
+    const isRegister = router.pathname === PATH.AUTH.REGISTER;
 
-	// Гость
-	if (user.role === ROLES.GUEST) {
-		return (
-			<div className="flex items-center space-x-2">
-				{!isSignIn && (
-					<Link
-						href={PATH.AUTH.SIGNIN}
-						className="btn bg-green-600 hover:bg-green-700 text-white px-3 py-1 rounded text-sm shadow-lg flex items-center cursor-pointer"
-					>
-						<AiOutlineHome className="mr-1" />
-						Login
-					</Link>
-				)}
-				{!isRegister && (
-					<Link
-						href={PATH.AUTH.REGISTER}
-						className="btn bg-blue-600  hover:bg-blue-700 text-white px-3 py-1 rounded text-sm shadow-lg flex items-center  cursor-pointer"
-					>
-						<AiOutlinePlusCircle className="mr-1" />
-						Registration
-					</Link>
-				)}
-			</div>
-		);
-	}
+    // Гость
+    if (user.role === ROLES.GUEST) {
+        return (
+            <div className="flex items-center space-x-2">
+                {!isSignIn && (
+                    <Link
+                        href={PATH.AUTH.SIGNIN}
+                        className="btn bg-green-600 hover:bg-green-700 text-white px-3 py-1 rounded text-sm shadow-lg flex items-center cursor-pointer"
+                    >
+                        <AiOutlineHome className="mr-1"/>
+                        Login
+                    </Link>
+                )}
+                {!isRegister && (
+                    <Link
+                        href={PATH.AUTH.REGISTER}
+                        className="btn bg-blue-600  hover:bg-blue-700 text-white px-3 py-1 rounded text-sm shadow-lg flex items-center  cursor-pointer"
+                    >
+                        <AiOutlinePlusCircle className="mr-1"/>
+                        Registration
+                    </Link>
+                )}
+            </div>
+        );
+    }
 
-	const logoutHandler = async () => {
-		await signOut({ callbackUrl: PATH.HOME.ROOT });
-	};
-	const editHandler = async () => {
-		if (!eventId) return;
-		await router.push(PATH.EVENTS.EDIT(eventId));
-	};
-	const createHandler = async () => {
-		await router.push(PATH.EVENTS.CREATE);
-	};
-	const canEdit =
-		isDetailPage &&
-		(user.role === ROLES.ADMIN || (user.role === ROLES.USER && user.id === eventAuthorId));
-	console.log(user.id, eventAuthorId);
-	return (
-		<div className="flex items-center space-x-2">
-			{hasRole(user.role, [ROLES.ADMIN, ROLES.USER]) && (
-				<div className="flex items-center gap-2">
-					{user.role === ROLES.ADMIN && !isDashboard && (
-						<Link
-							href={PATH.ADMIN.DASHBOARD}
-							className="ml-4 btn bg-gray-600 hover:bg-gray-700 text-white px-3 py-1 rounded text-sm shadow-lg flex items-center"
-						>
-							<AiOutlineDashboard className="mr-1" />
-							Dashboard
-						</Link>
-					)}
-					{(isHome || isDetailPage) && (
-						<ButtonEventAction
-							type={BUTTON_EVENT_TYPE.CREATE}
-							className="flex items-center gap-1 cursor-pointer"
-							onClick={createHandler}>
-							<AiOutlinePlusCircle />
-							<span>Create</span>
-						</ButtonEventAction>
+    const logoutHandler = async () => {
+        await signOut({callbackUrl: PATH.HOME.ROOT});
+    };
+    const editHandler = async () => {
+        if (!eventId) return;
+        await router.push(PATH.EVENTS.EDIT(eventId));
+    };
+    const createHandler = async () => {
+        await router.push(PATH.EVENTS.CREATE);
+    };
+    const canEdit =
+        isDetailPage &&
+        (user.role === ROLES.ADMIN || (user.role === ROLES.USER && user.id === eventAuthorId));
+    console.log(user.id, eventAuthorId);
+    return (
+        <div className="flex items-center space-x-2">
+            {hasRole(user.role, [ROLES.ADMIN, ROLES.USER]) && (
+                <div className="flex items-center gap-2">
+                    {user.role === ROLES.ADMIN && !isDashboard && (
+                        <Link
+                            href={PATH.ADMIN.DASHBOARD}
+                            className="ml-4 btn bg-gray-600 hover:bg-gray-700 text-white px-3 py-1 rounded text-sm shadow-lg flex items-center"
+                        >
+                            <AiOutlineDashboard className="mr-1"/>
+                            Dashboard
+                        </Link>
+                    )}
+                    {(isHome || isDetailPage) && (
+                        <ButtonEventAction
+                            type={BUTTON_EVENT_TYPE.CREATE}
+                            className="flex items-center gap-1 cursor-pointer"
+                            onClick={createHandler}>
+                            <AiOutlinePlusCircle/>
+                            <span>Create</span>
+                        </ButtonEventAction>
 
-					)}
-					{canEdit && (
-						<ButtonEventAction
-							type={BUTTON_EVENT_TYPE.EDIT}
-							className="flex items-center gap-1 cursor-pointer"
-							onClick={editHandler}>
-							<AiOutlineEdit />
-							<span>Edit</span>
-						</ButtonEventAction>
-					)}
-					<button
-						onClick={logoutHandler}
-						className="btn bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded text-sm shadow-lg flex items-center  cursor-pointer"
-					>
-						<AiOutlineLogout className="mr-1" />
-						Logout ({user.name})
-					</button>
+                    )}
+                    {canEdit && (
+                        <ButtonEventAction
+                            type={BUTTON_EVENT_TYPE.EDIT}
+                            className="flex items-center gap-1 cursor-pointer"
+                            onClick={editHandler}>
+                            <AiOutlineEdit/>
+                            <span>Edit</span>
+                        </ButtonEventAction>
+                    )}
+                    <button
+                        onClick={logoutHandler}
+                        className="btn bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded text-sm shadow-lg flex items-center  cursor-pointer"
+                    >
+                        <AiOutlineLogout className="mr-1"/>
+                        Logout ({user.name})
+                    </button>
 
-				</div>
-			)}
-		</div>
-	);
+                </div>
+            )}
+        </div>
+    );
 }
--- react_2/8_5_next_page_db/src/components/ui/Layout.tsx
+++ react_2/8_5_next_page_db/src/components/ui/Layout.tsx
@@ -1,30 +1,30 @@
-import { ReactNode } from 'react';
+import {ReactNode} from 'react';
 import Link from 'next/link';
 import ButtonHeader from './ButtonHeader';
-import { useUserRole } from '@/server/roles';
-import { AiOutlineHome } from 'react-icons/ai';
-import { PATH } from '@/shared/path';
+import {useUserRole} from '@/server/core/roles';
+import {AiOutlineHome} from 'react-icons/ai';
+import {PATH} from '@/shared/path';
 
 interface LayoutProps {
-	children: ReactNode;
-	eventAuthorId?: number;
+    children: ReactNode;
+    eventAuthorId?: number;
 }
 
-export const Layout =  ({ children, eventAuthorId }: LayoutProps) => {
-	const user = useUserRole();
-	return (
-		<div className="min-h-screen flex flex-col">
-			<header className="p-4 bg-white shadow-md flex justify-between items-center">
-				<div>
-					<Link href={PATH.HOME.ROOT}
-						  className="text-2xl text-gray-700 hover:text-gray-900 text-shadow lg">
-						<AiOutlineHome />
-					</Link>
-				</div>
-				<ButtonHeader user={user} eventAuthorId={eventAuthorId}/>
-			</header>
+export const Layout = ({children, eventAuthorId}: LayoutProps) => {
+    const user = useUserRole();
+    return (
+        <div className="min-h-screen flex flex-col">
+            <header className="p-4 bg-white shadow-md flex justify-between items-center">
+                <div>
+                    <Link href={PATH.HOME.ROOT}
+                          className="text-2xl text-gray-700 hover:text-gray-900 text-shadow lg">
+                        <AiOutlineHome/>
+                    </Link>
+                </div>
+                <ButtonHeader user={user} eventAuthorId={eventAuthorId}/>
+            </header>
 
-			<main className="flex-1 p-4">{children}</main>
-		</div>
-	);
+            <main className="flex-1 p-4">{children}</main>
+        </div>
+    );
 };
