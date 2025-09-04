cbf3d316fe69f700880023d70ff2cfdf5833c88d
maxvanny2010 <mailtime2010@gmail.com>
2025-09-04 06:01:30 -0500

    m7.ssr.next.page.db.nextauth

--- react_2/8_5_next_page_db/src/pages/api/auth/[...nextauth].ts
+++ react_2/8_5_next_page_db/src/pages/api/auth/[...nextauth].ts
@@ -1,59 +1,60 @@
 import CredentialsProvider from 'next-auth/providers/credentials';
 import NextAuth from 'next-auth/next';
 import prisma from '@/server/core/db';
-import {NextAuthOptions} from 'next-auth';
+import { NextAuthOptions } from 'next-auth';
 import bcrypt from 'bcryptjs';
-import {RoleType} from '@/shared/types';
+import { RoleType } from '@/shared/types';
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                email: {label: 'Username', type: 'text'},
-                password: {label: 'Password', type: 'password'},
-            },
-            async authorize(credentials, req): Promise<any> {
-                if (!credentials) return null;
+	providers: [
+		CredentialsProvider({
+			name: 'Credentials',
+			credentials: {
+				email: { label: 'Username', type: 'text' },
+				password: { label: 'Password', type: 'password' },
+			},
+			async authorize(credentials) {
+				if (!credentials) return null;
 
-                const user = await prisma.user.findUnique({
-                    where: {
-                        email: credentials.email,
-                        deleted: false,
-                    }
-                });
-                if (!user) return null;
+				const user = await prisma.user.findUnique({
+					where: {
+						email: credentials.email,
+						deleted: false,
+					},
+				});
+				if (!user) return null;
 
-                const isValid = await bcrypt.compare(credentials.password, user.password);
-                if (!isValid) return null;
+				const isValid = await bcrypt.compare(credentials.password, user.password);
+				if (!isValid) return null;
 
-                return {
-                    id: Number(user.id),
-                    name: user.name,
-                    email: user.email,
-                    role: user.role,
-                };
-            },
+				return {
+					id: String(user.id),
+					name: user.name,
+					email: user.email,
+				} as never;
+			},
 
-        }),
-    ],
-    callbacks: {
-        async session({session, token}) {
-            // Добавляю роль из JWT в сессию
-            if (session.user) {
-                session.user.id = token.id;
-                session.user.role = token.role as RoleType;
-            }
-            return session;
-        },
-        async jwt({token, user}) {
-            if (user) {
-                token.id = Number(user.id);
-                token.role = user.role as RoleType; // сохраняю роль в JWT
-            }
-            return token;
-        },
-    },
+		}),
+	],
+	callbacks: {
+		async session({ session, token }) {
+			if (session.user) {
+				session.user.id = token.id;
+				session.user.role = token.role as RoleType;
+			}
+			return session;
+		},
+		async jwt({ token, user }) {
+			if (user) {
+				const dbUser = await prisma.user.findUnique({ where: { id: Number(user.id) } });
+				if (dbUser) {
+					token.role = dbUser.role as RoleType;
+				}
+				token.id = Number(user.id);
+			}
+			return token;
+		},
+	},
 };
 
 export default NextAuth(authOptions);
