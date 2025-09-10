1bda4ff79768d177198e8074954fa990b3229870
maxvanny2010 <mailtime2010@gmail.com>
2025-09-10 06:26:05 -0500

    m9.docker.env

--- docker/index.js
+++ docker/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { default: fetch } = require('node-fetch');
 const app = express();
+
+const DATE_SERVER_HOST = process.env.DATE_SERVER_HOST || 'http://localhost:3005';
+
 app.get('/', (req, res) => {
-	fetch('http://date:3005')
+	console.log(DATE_SERVER_HOST);
+	fetch(DATE_SERVER_HOST)
 		.then(res => res.json())
 		.then(date => res.send(`Hello! Current date: ${date}\n`));
 });
